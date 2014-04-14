bfb9c5beff0c182b627f16e584067ee909f12349
Dzenan Ridjanovic <dzenanr@gmail.com>
2014-04-14 10:47:49 -0400

    update gitignore, build and pubspec

--- build.dart
+++ build.dart
@@ -1,5 +1,6 @@
 import 'package:polymer/builder.dart';
-        
-main() {     
-  build(entryPoints: ['web/polymer_project_tasks.html']);
+
+main(args) {
+  build(entryPoints: ['web/polymer_project_tasks.html'],
+        options: parseOptions(args));
 }
